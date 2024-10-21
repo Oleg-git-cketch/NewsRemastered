6cd7ec59a984d23fd9f2d80a88cdb28ccac4b755
Oleg-git-cketch <aliol439003@gmail.com>
2024-10-21 21:42:50 +0500

    First commit

--- index/models.py
+++ index/models.py
@@ -12,19 +12,8 @@ class Category(models.Model):
 class Product(models.Model):
     product_name = models.CharField(max_length=128)
     product_description = models.TextField()
-    product_price = models.FloatField()
-    product_count = models.IntegerField()
     product_photo = models.ImageField(upload_to='media')
     product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.product_name)
-
-
-class Cart(models.Model):
-    user_id = models.IntegerField()
-    user_product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    user_product_quantity = models.IntegerField()
-
-    def __str__(self):
-        return str(self.user_id)
